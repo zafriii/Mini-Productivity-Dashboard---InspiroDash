ed225ed018650509e10cddf5de937a4ddf029cbe
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2025-06-13 14:14:41 +0600

    Update taskModel.js

--- backend/server/models/taskModel.js
+++ backend/server/models/taskModel.js
@@ -1,27 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const taskSchema = new mongoose.Schema({
-
-//   content: {
-//     type: String,
-//     required: true,
-//   },
-//   date: {
-//     type: String,
-//     required: true,
-//   },
-//   user: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true,  // Associates the note with the logged-in user
-//   },
-// }, { timestamps: true });
-
-// const Task = mongoose.model('Task', taskSchema);
-
-// module.exports = Task;
-
-
 
 const mongoose = require('mongoose');
 
